3c2a0a00f12bb159ecadadf0c8b92516f0733158
jackchen10120417 <jackchen1012@gmail.com>
2026-06-27 13:53:25 +0800

    Update send_msg.py
    新增 時間

--- send_msg.py
+++ send_msg.py
@@ -1,13 +1,23 @@
 import os
 import requests
+from datetime import datetime, timezone, timedelta
 
 def send_telegram_message():
     # 這裡會自動讀取你剛剛在 Secrets 設定的值
     token = os.environ.get('TELEGRAM_TOKEN')
     chat_id = os.environ.get('TELEGRAM_CHAT_ID')
+ 
+     # 取得台灣時間 (UTC+8)新增
+    taiwan_tz = timezone(timedelta(hours=8))
+    now = datetime.now(taiwan_tz)
+    time_str = now.strftime("%Y-%m-%d %H:%M:%S")    
     
     # 你的訊息內容
-    message = "這是一則來自 GitHub Actions 的自動排程訊息！"
+    #message = "這是一則來自 GitHub Actions 的自動排程訊息！"
+
+    # 訊息內容加入時間--新增
+    message = f"自動排程通知\n現在時間: {time_str}"
+    
     
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
@@ -17,7 +27,9 @@ def send_telegram_message():
     
     response = requests.post(url, data=payload)
     if response.status_code == 200:
-        print("訊息發送成功")
+        #新增
+        print(f"訊息發送成功: {time_str}")
+      # print("訊息發送成功")
     else:
         print(f"發送失敗: {response.text}")
 
